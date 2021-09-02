c4c842f9f6a2974f1dfdaeba00b0fe6972c9848c
E1Hephaestus <gefestroyale@gmail.com>
2021-09-02 20:06:04 +0300

    Exception implemented

--- include/common/log.hpp
+++ include/common/log.hpp
@@ -4,6 +4,7 @@
 #include "fmt/core.h"
 #include "fmt/color.h"
 #include <utility>
+#include <exception>
 
 #if defined(CORTEX_SERVER)
     #define CORTEX_TYPE_NAME "Server"
@@ -38,6 +39,20 @@ inline void Println(const char *fmt, ArgsType&&...args){
     fmt::print(fmt, std::forward<ArgsType>(args)...);
     fmt::print("\n");
 }
+
+class Exception: public std::exception{
+private:
+    std::string m_Message;
+public:
+    template<typename ...ArgsType>
+    Exception(const char *fmt, ArgsType&&...args){
+        m_Message = fmt::format(fmt, std::forward<ArgsType>(args)...);
+    }
+
+    const char *what()const noexcept override{
+        return m_Message.c_str();
+    }
+};
 
 
 #endif//CORTEX_COMMON_LOG_HPP
